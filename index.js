aa03cf5268e9375d522545fc35e6eae510b85376
Krister Haverinen <krister@piilossa.fi>
2021-08-19 21:35:11 +0300

    Completed osa  3 step9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('body', function (req, res) {
@@ -8,6 +9,7 @@ morgan.token('body', function (req, res) {
   }
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -92,7 +94,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })

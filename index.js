891d0928d30c471159170fc958df55e8d6a637cc
Krister Haverinen <krister@piilossa.fi>
2021-08-27 16:48:02 +0300

    Completed 3.18

--- index.js
+++ index.js
@@ -11,78 +11,90 @@ morgan.token('body', function (req, res) {
   }
 })
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(errorHandler)
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/info', (req, res) => {
-  const count = persons.length
-  const date = new Date()
-  res.send(`Phonebook has ${count} entries<br/>${date.toString()}`)
-})
-
-app.get('/api/persons', (req, res) => {
-  Person
-    .find({})
+app.get('/info', (request, response) => {
+  Person.find({})
     .then(persons => {
-      res.json(persons)
+      const count = persons.length
+      const date = new Date()
+      response.send(`Phonebook has ${count} entries<br/>${date.toString()}`)
     })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person
-  .find({_id: request.params.id})
-  .then(result => {
-    if (result) {
-      response.json(result)
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => console.log(error))
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then(persons => {
+      response.json(persons)
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
-  const generateId = () => {
-    return Math.floor(Math.random() * 1000);
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(result => {
+      if (result) {
+        response.json(result)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
 
-  if (!request.body || !request.body.name || !request.body.number) {
+app.post('/api/persons', (request, response, next) => {
+ const body = request.body
+
+  if (!body.name || !body.number) {
     return response.status(400).json({
       error: 'Content missing. Check name and number.'
     })
   }
 
-  const newPerson = request.body
+  const newEntry = new Person(body)
 
-  Person
-    .find({name: newPerson.name})
-    .then(result => {
-      if (result.length) {
-        response.status(409).json({
-          error: 'Name already exists.'
-        })
-      }
-      else {
-        const newEntry = new Person(newPerson)
-
-        newEntry
-          .save()
-          .then(result => response.json({'message': `${newPerson.name} saved!`}))
-      }
-    })
+  newEntry.save()
+    .then(result => response.json(result))
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(p => p.id !== id)
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => response.status(204).end())
+    .catch(error => next(error))
+})
 
-  response.status(204).end()
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  console.log(body)
+
+  const entry = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, entry, {new: true})
+    .then(result => response.json(result))
+    .catch(error => next(error))
 })
 
 const PORT = process.env.PORT || 3001
